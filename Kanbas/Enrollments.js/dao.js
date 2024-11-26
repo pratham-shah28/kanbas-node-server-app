21f4450bd8a91738434927358177c4638110d6f7
pratham-shah28 <psshah2803@gmail.com>
2024-11-26 02:41:20 -0500

    till 4.5

---
+++ Kanbas/Enrollments.js/dao.js
@@ -0,0 +1,6 @@
+export function createCourse(course) {
+    const newCourse = { ...course, _id: Date.now().toString() };
+    Database.courses = [...Database.courses, newCourse];
+    return newCourse;
+  }
+  
